80af4091640b03aa100da39fcb6f9d8911779ca0
kasznar <kasznardavid8@gmail.com>
2020-02-11 19:12:13 +0100

    new firebase config

--- cordova/platforms/android/app/src/main/java/hu/kasznar/hellocordova/MainActivity.java
+++ cordova/platforms/android/app/src/main/java/hu/kasznar/hellocordova/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package io.cordova.hellocordova;
+package hu.kasznar.hellocordova;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
--- cordova/platforms/ios/platform_www/cordova_plugins.js
+++ cordova/platforms/ios/platform_www/cordova_plugins.js
@@ -10,7 +10,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4",
     "phonegap-plugin-push": "2.3.0"
   };
 });
